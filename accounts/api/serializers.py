32502d10403b2c21f715ec3042ebc32cf883cf91
Peng Liu <winner245@hotmail.com>
2021-11-27 21:22:24 -0500

    Add login, login status, logout, sign-up APIs

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,8 +1,52 @@
-from django.contrib.auth.models import User, Group
-from rest_framework import serializers
+from django.contrib.auth.models import User
+from rest_framework import serializers, exceptions
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email']
+        fields = ('username', 'email')
+
+
+class SignupSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(max_length=20, min_length=6)
+    password = serializers.CharField(max_length=20, min_length=6)
+    email = serializers.EmailField()
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+    # will be called by SignupSerializer.is_valid()
+    def validate(self, data):
+        # TODO<HOMEWORK> 增加验证 username 是不是只由给定的字符集合构成
+        if User.objects.filter(username=data['username'].lower()).exists():
+            raise exceptions.ValidationError({
+                'message': 'This username has been occupied.'
+            })
+        if User.objects.filter(email=data['email'].lower()).exists():
+            raise exceptions.ValidationError({
+                'message': 'This email address has been occupied.'
+            })
+        return data
+
+    def create(self, validated_data):
+        username = validated_data['username'].lower()
+        email = validated_data['email'].lower()
+        password = validated_data['password']
+
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password, # create_user 内部会调用 _create_user() -> set_password() -> make_password() => 存储的是密文密码！
+        )
+
+        # 下面的方式则是用 ORM 插入明文密码，这是绝对不推荐的！一定要用 Django 提供的 create_user 来存储密文密码
+        #User.objects.create(username=username, email=email, password=password)
+
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
